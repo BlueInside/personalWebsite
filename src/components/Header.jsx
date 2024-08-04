61f8105424c0d066ff046f98cf34ee5c481b40eb
Karol <kpulawski13@gmail.com>
2024-08-04 11:54:38 +0100

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="fixed z-10 w-full bg-gray-800 p-4">
+    <header className="z-10 w-full bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-lg font-bold text-white">Karol Pulawski</div>
         <Navbar />
