975f0f3a749c1197a7dc93f6ea0ac509223312b9
Karol <kpulawski13@gmail.com>
2024-08-06 07:32:00 +0100

    Add socials to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,38 @@
 import Navbar from "./Navbar";
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
 
 export default function Header() {
   return (
     <header className="fixed z-20 w-full bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-lg font-bold text-white">Karol Pulawski</div>
+        <div className="flex gap-10">
+          <a
+            href={"https://github.com/BlueInside"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-600"
+          >
+            <FaGithub size={34} />
+          </a>
+          <a
+            href={"https://www.linkedin.com/in/karol-pulawski-66712b1b6/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-600"
+          >
+            <FaLinkedinIn size={34} />
+          </a>
+          <a
+            href={"https://www.instagram.com/karolessence/"}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white hover:text-gray-600"
+          >
+            <FaInstagram size={34} />
+          </a>
+        </div>
+
         <Navbar />
       </div>
     </header>
